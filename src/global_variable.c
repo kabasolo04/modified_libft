e4ded50302cdc1f6744a6ec1624c932b661959b9
Koldo Abasolo Iturregi <koldobikaabasolo@gmail.com>
2024-04-24 11:47:24 +0200

    BuenoBonitoBarato

---
+++ src/global_variable.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   global_variable.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kabasolo <kabasolo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/24 11:33:57 by kabasolo          #+#    #+#             */
+/*   Updated: 2024/04/24 11:46:23 by kabasolo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*global_variable(void *var)
+{
+	static void	*g_var;
+
+	if (var)
+		g_var = var;
+	if (!g_var)
+		g_var = NULL;
+	return (g_var);
+}
